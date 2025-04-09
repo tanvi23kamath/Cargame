cbe94806efd8a02505a9fed101e83c320e3c64c3
Tanvi <tanvikamath23@gmail.com>
2025-04-09 21:47:06 +0530

    First commit

---
+++ game.py
@@ -0,0 +1,209 @@
+import pygame, random, sys ,os,time
+from pygame.locals import *
+
+WINDOWWIDTH = 800
+WINDOWHEIGHT = 600
+TEXTCOLOR = (255, 255, 255)
+BACKGROUNDCOLOR = (0, 0, 0)
+FPS = 40
+CARMINSIZE = 10
+CARMAXSIZE = 40
+CARMINSPEED = 8
+CARMAXSPEED = 8
+ADDNEWCARRATE = 6
+PLAYERMOVERATE = 5
+count=3
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def waitForPlayerToPressKey():
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                terminate()
+            if event.type == KEYDOWN:
+                if event.key == K_ESCAPE: #escape quits
+                    terminate()
+                return
+
+def playerHasHitCar(playerRect, cars):
+    for b in cars:
+        if playerRect.colliderect(b['rect']):
+            return True
+    return False
+
+def drawText(text, font, surface, x, y):
+    textobj = font.render(text, 1, TEXTCOLOR)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
+
+# set up pygame, the window, and the mouse cursor
+pygame.init()
+mainClock = pygame.time.Clock()
+windowSurface = pygame.display.set_mode((WINDOWWIDTH, WINDOWHEIGHT))
+pygame.display.set_caption('car race')
+pygame.mouse.set_visible(False)
+
+# fonts
+font = pygame.font.SysFont(None, 30)
+
+
+
+# images
+playerImage = pygame.image.load('image/car5.png')
+car3 = pygame.image.load('image/car3.png')
+playerRect = playerImage.get_rect()
+carImage = pygame.image.load('image/car2.png')
+sample = [car3,carImage]
+wallLeft = pygame.image.load('image/left.png')
+wallRight = pygame.image.load('image/right.png')
+
+
+# "Start" screen
+drawText('Press any key to start the game.', font, windowSurface, (WINDOWWIDTH / 3) - 30, (WINDOWHEIGHT / 3))
+drawText('And Enjoy', font, windowSurface, (WINDOWWIDTH / 3), (WINDOWHEIGHT / 3)+30)
+pygame.display.update()
+waitForPlayerToPressKey()
+zero=0
+if not os.path.exists("data/save.dat"):
+    f=open("data/save.dat",'w')
+    f.write(str(zero))
+    f.close()   
+v=open("data/save.dat",'r')
+topScore = int(v.readline())
+v.close()
+while (count>0):
+    # start of the game
+    cars = []
+    score = 0
+    playerRect.topleft = (WINDOWWIDTH / 2, WINDOWHEIGHT - 50)
+    moveLeft = moveRight = moveUp = moveDown = False
+    reverseCheat = slowCheat = False
+    carAddCounter = 0
+ 
+
+    while True: # the game loop
+        score += 1 # increase score
+
+        for event in pygame.event.get():
+            
+            if event.type == QUIT:
+                terminate()
+
+            if event.type == KEYDOWN:
+                if event.key == ord('z'):
+                    reverseCheat = True
+                if event.key == ord('x'):
+                    slowCheat = True
+                if event.key == K_LEFT or event.key == ord('a'):
+                    moveRight = False
+                    moveLeft = True
+                if event.key == K_RIGHT or event.key == ord('d'):
+                    moveLeft = False
+                    moveRight = True
+                if event.key == K_UP or event.key == ord('w'):
+                    moveDown = False
+                    moveUp = True
+                if event.key == K_DOWN or event.key == ord('s'):
+                    moveUp = False
+                    moveDown = True
+
+            if event.type == KEYUP:
+                if event.key == ord('z'):
+                    reverseCheat = False
+                    score = 0
+                if event.key == ord('x'):
+                    slowCheat = False
+                    score = 0
+                if event.key == K_ESCAPE:
+                        terminate()
+            
+
+                if event.key == K_LEFT or event.key == ord('a'):
+                    moveLeft = False
+                if event.key == K_RIGHT or event.key == ord('d'):
+                    moveRight = False
+                if event.key == K_UP or event.key == ord('w'):
+                    moveUp = False
+                if event.key == K_DOWN or event.key == ord('s'):
+                    moveDown = False
+
+            
+
+        # Add new baddies at the top of the screen
+        if not reverseCheat and not slowCheat:
+            carAddCounter += 1
+        if carAddCounter == ADDNEWCARRATE:
+            carAddCounter = 0
+            carSize =30 
+            newCar = {'rect': pygame.Rect(random.randint(140, 485), 0 - carSize, 23, 47),
+                        'speed': random.randint(CARMINSPEED, CARMAXSPEED),
+                        'surface':pygame.transform.scale(random.choice(sample), (23, 47)),
+                        }
+            cars.append(newCar)
+            sideLeft= {'rect': pygame.Rect(0,0,126,600),
+                       'speed': random.randint(CARMINSPEED, CARMAXSPEED),
+                       'surface':pygame.transform.scale(wallLeft, (126, 599)),
+                       }
+            cars.append(sideLeft)
+            sideRight= {'rect': pygame.Rect(497,0,303,600),
+                       'speed': random.randint(CARMINSPEED, CARMAXSPEED),
+                       'surface':pygame.transform.scale(wallRight, (303, 599)),
+                       }
+            cars.append(sideRight)
+            
+            
+
+        # Move the player around.
+        if moveLeft and playerRect.left > 0:
+            playerRect.move_ip(-1 * PLAYERMOVERATE, 0)
+        if moveRight and playerRect.right < WINDOWWIDTH:
+            playerRect.move_ip(PLAYERMOVERATE, 0)
+        if moveUp and playerRect.top > 0:
+            playerRect.move_ip(0, -1 * PLAYERMOVERATE)
+        if moveDown and playerRect.bottom < WINDOWHEIGHT:
+            playerRect.move_ip(0, PLAYERMOVERATE)
+        
+        for b in cars:
+            if not reverseCheat and not slowCheat:
+                b['rect'].move_ip(0, b['speed'])
+            elif reverseCheat:
+                b['rect'].move_ip(0, -5)
+            elif slowCheat:
+                b['rect'].move_ip(0, 1)
+
+         
+        for b in cars[:]:
+            if b['rect'].top > WINDOWHEIGHT:
+                cars.remove(b)
+
+        # Draw the game world on the window.
+        windowSurface.fill(BACKGROUNDCOLOR)
+
+        # Draw the score and top score.
+        drawText('Score: %s' % (score), font, windowSurface, 128, 0)
+        drawText('Top Score: %s' % (topScore), font, windowSurface,128, 20)
+        
+        windowSurface.blit(playerImage, playerRect)
+
+        
+        for b in cars:
+            windowSurface.blit(b['surface'], b['rect'])
+
+        pygame.display.update()
+
+        # Check if any of the car have hit the player.
+        if playerHasHitCar(playerRect, cars):
+            if score > topScore:
+                g=open("data/save.dat",'w')
+                g.write(str(score))
+                g.close()
+                topScore = score
+            break
+
+        mainClock.tick(FPS)
+
+    
